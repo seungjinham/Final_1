954ce45f1ee7f61f19fd02a8fe143981b0c7c91f
JJH <wognstm5@gmail.com>
2018-01-31 21:30:42 +0900

    930

--- src/main/java/com/iu/company/CompanyDAO.java
+++
@@ -1,8 +0,0 @@
-package com.iu.company;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class CompanyDAO {
-
-}
--- src/main/java/com/iu/company/CompanyService.java
+++
@@ -1,8 +0,0 @@
-package com.iu.company;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CompanyService {
-
-}
