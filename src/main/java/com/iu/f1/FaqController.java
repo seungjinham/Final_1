540a2d2a2cf2e91adb4c8dee00941b3d47950267
gpffh1215 <gpffh1215@gmail.com>
2018-02-01 21:28:05 +0900

    roger

--- src/main/java/com/iu/f1/FaqController.java
+++ src/main/java/com/iu/f1/FaqController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,5 +34,57 @@ public class FaqController {
 		return mv;
 	
 	}	
+	
+	@RequestMapping(value="faqWrite", method=RequestMethod.GET)
+	public void insert(Model model) throws Exception{
+		
+	}
+	
+	@RequestMapping(value="faqWrite", method=RequestMethod.POST)
+	public String insert(FaqDTO faqDTO) throws Exception{
+		int result=faqService.insert(faqDTO);
+		String message="등록 오류";
+		if(result>0){
+			message="등록 성공";
+		}
+		return "redirect:./faqList";
+	}
+	
+	
+	@RequestMapping(value="faqDelete", method=RequestMethod.GET)
+	public String delete(int num) throws Exception{
+		int result= faqService.delete(num);
+		
+		return "redirect:./faqList"; 
+	}
+	
+	@RequestMapping(value="faqUpdate", method=RequestMethod.GET)
+	public void update()throws Exception{
+		
+	}
+	
+	@RequestMapping(value="faqUpdate", method=RequestMethod.POST)
+	public ModelAndView update(int num) throws Exception{
+		FaqDTO faqDTO= faqService.selectOne(num);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("view", faqDTO);
+		faqService.update(faqDTO);
+		return mv;
+	}
+	
+	@RequestMapping(value="faqView", method=RequestMethod.GET)
+	public ModelAndView selectOne(int num)throws Exception{
+		FaqDTO faqDTO = faqService.selectOne(num);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("view", faqDTO);
+		mv.setViewName("faq/faqView");
+		return mv;
+	}
+	
+	
+	
+	
+	
 
 }
